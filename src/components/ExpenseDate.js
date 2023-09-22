f677e031c09328451e270bafa9efac597a712259
qjstuart <quentinqjs@gmail.com>
2023-09-22 12:10:20 +0200

    Change to arrow functions and format code

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,9 @@
-import './ExpenseDate.css'
+import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
-  const day = props.date.toLocaleString('en-US', { month: 'long' })
-  const month = props.date.toLocaleString('en-US', { day: '2-digit' })
-  const year = props.date.getFullYear()
+const ExpenseDate = (props) => {
+  const day = props.date.toLocaleString("en-US", { month: "long" });
+  const month = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
@@ -11,7 +11,7 @@ function ExpenseDate(props) {
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseDate
+export default ExpenseDate;
