cd6015c022049070862e9b1cdc8cbd7645ad4ae9
qjstuart <quentinqjs@gmail.com>
2023-09-24 13:08:25 +0200

    Added form to submit a new expense

---
+++ src/components/NewExpense/NewExpenseItem.js
@@ -0,0 +1,12 @@
+import "./NewExpenseItem.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpenseItem = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpenseItem;
